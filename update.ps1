0952d17b5bb22bf40610593054c77764462600db
Tony Stark <73957207+TonyStark-47@users.noreply.github.com>
2025-04-18 23:45:52 +0530

    Add setup and update script for linux/macos and windows.

---
+++ update.ps1
@@ -0,0 +1,4 @@
+cd "$env:USERPROFILE\Extension-Job-Application-Tracker"
+git pull origin main
+Write-Host "Extension updated. Reload it in chrome://extensions if needed."
+
---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~/Extension-Job-Application-Tracker || exit
+git pull origin main
+echo "Extension updated. Reload it in chrome://extensions if needed."
+
